c9d82df4a3164d690f483f67017ddc8d578a8c03
changrae kang <changraekang@changraeui-MacBookAir.local>
2022-03-29 18:18:25 +0900

    postgreSQL 연결

--- django__prj/settings.py
+++ django__prj/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'django__prj.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': { 'ENGINE': 'django.db.backends.postgresql', 
+                'NAME': 'DjangoStudy', 
+                'USER': 'postgres', 
+                'PASSWORD' : '1234', 
+                'HOST' : 'localhost', 
+                'PORT' :'5432', }
 }
 
 
